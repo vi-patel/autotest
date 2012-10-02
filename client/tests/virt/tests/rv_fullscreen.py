47d276fe6153f5ac99962ff9796214c400b8daf1
Vimal Patel <vipatel@redhat.com>
2012-10-02 17:15:31 -0400

    Changing the messages, cleaning up resolution, & other minor fixes.
    Signed-off-by: Vimal Patel <vipatel@redhat.com>

--- client/tests/virt/tests/rv_fullscreen.py
+++ client/tests/virt/tests/rv_fullscreen.py
@@ -8,14 +8,15 @@ Requires: connected binaries remote-viewer, Xorg, gnome session
 
 """
 import logging, os
+from virttest.aexpect import ShellCmdError
 from autotest.client.shared import error
 
 
 def run_rv_fullscreen(test, params, env):
     """
     Tests the --full-screen option
-    Positive test: full_screen parameter = True
-    Negative test: full_screen parameter = False
+    Positive test: full_screen param = yes, verify guest res = client res
+    Negative test: full_screen param= no, verify guest res != client res
 
     @param test: KVM test object.
     @param params: Dictionary with the test parameters.
@@ -37,36 +38,46 @@ def run_rv_fullscreen(test, params, env):
     # Get the resolution of the client & guest
     logging.info("Getting the Resolution on the client")
     client_session.cmd("export DISPLAY=:0.0")
-    client_res = client_session.cmd("xrandr | grep '*'")
-
-    logging.info("Getting the Resolution on the guest")
 
     try:
-        guest_session.cmd("startx &")
-    except:
-        logging.debug("Ignoring an Exception that Occurs from calling startx")
+        client_res_raw = client_session.cmd("xrandr | grep '*'")
+        client_res = client_res_raw.split()[0]
+    except ShellCmdError:
+        raise error.TestFail("Could not get guest resolution, xrandr output:" +
+                             " %s" % client_res_raw)
+    except IndexError:
+        raise error.TestFail("Could not get guest resolution, xrandr output:" +
+                             " %s" % client_res_raw)
 
+    logging.info("Getting the Resolution on the guest")
     guest_session.cmd("export DISPLAY=:0.0")
-    guest_res = guest_session.cmd("xrandr | grep '*'")
+
+    try:
+        guest_res_raw = guest_session.cmd("xrandr | grep '*'")
+        guest_res = guest_res_raw.split()[0]
+    except ShellCmdError:
+        raise error.TestFail("Could not get guest resolution, xrandr output:" +
+                             " %s" % guest_res_raw)
+    except IndexError:
+        raise error.TestFail("Could not get guest resolution, xrandr output:" +
+                             " %s" % guest_res_raw)
 
     logging.info("Here's the information I have: ")
     logging.info("\nClient Resolution: " + client_res)
     logging.info("\nGuest Resolution: " + guest_res)
-    logging.info("\nFormatted Client Resolution: " + client_res.split()[0])
-    logging.info("\nFormatted Guest Resolution: " + guest_res.split()[0])
 
     # Positive Test, verify the guest takes the resolution of the client
-    if full_screen == "True":
-        if(client_res.split()[0] == guest_res.split()[0]):
-            logging.info("PASS: GUEST RESOLUTION IS THE SAME AS THE CLIENT")
+    if full_screen == "yes":
+        if(client_res == guest_res):
+            logging.info("PASS: Guest resolution is the same as the client")
         else:
-            raise error.TestFail("GUEST RESOLUTION DIFFERS FROM THE CLIENT")
+            raise error.TestFail("Guest resolution differs from the client")
     # Negative Test, verify the resolutions are not equal
-    elif full_screen == "False":
-        if(client_res.split()[0] != guest_res.split()[0]):
-            logging.info("PASS: GUEST RESOLUTION DIFFERS FROM THE CLIENT")
+    elif full_screen == "no":
+        if(client_res != guest_res):
+            logging.info("PASS: Guest resolution differs from the client")
         else:
-            raise error.TestFail("GUEST RESOLUTION IS THE SAME AS THE CLIENT")
+            raise error.TestFail("Guest resolution is the same as the client")
     else:
         raise error.TestFail("The test setup is incorrect.")
 
